14c87459a37bda4815d7dcb8f7bb17eb42bf32eb
Julio Cesar <julio_romero1426@hotmail.com>
2016-08-09 17:01:21 -0500

    layout te contacto

--- app/src/main/java/com/think/petagram2/Contacto.java
+++ app/src/main/java/com/think/petagram2/Contacto.java
@@ -2,10 +2,16 @@ package com.think.petagram2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.ActionMenuItemView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Contacto extends AppCompatActivity {
-
+    private Button enviarMail;
+    private EditText nombre,email,mensaje;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +20,18 @@ public class Contacto extends AppCompatActivity {
         setSupportActionBar(actionBar);
         getSupportActionBar().setTitle(R.string.menuCont);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        incializarElementosGUI();
+        enviarMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Contacto.this, email.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void incializarElementosGUI() {
+        enviarMail = (Button)findViewById(R.id.enviarMail);
+        nombre     = (EditText)findViewById(R.id.input_name);
+        email      = (EditText)findViewById(R.id.input_email);
+        mensaje    = (EditText)findViewById(R.id.input_message);
     }
 }
